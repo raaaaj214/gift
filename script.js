c84cfea980a791194c1a9c04c4bb7de716250d86
Raj Deshmukh <rajdeshmukh41705@gmail.com>
2024-10-28 23:38:44 +0530

    gift prepared

---
+++ script.js
@@ -0,0 +1,27 @@
+let form = document.querySelector("form");
+let input = document.querySelector("input");
+let div1 = document.querySelector(".div1");
+let div2 = document.querySelector(".div2");
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("Form submitted"); // Log form submission
+    if (input.value.toLowerCase() === "slowpoke") {
+        div1.style.display = "none";
+        div2.style.display = "flex";
+        
+setTimeout(() => {
+    let video = document.querySelector("#video");
+    if (video) {
+        video.play();
+        console.log("Video is playing");
+    } else {
+        console.log("Video element not found");
+    }
+}, 2000);
+    } else {
+        alert("Please enter the correct name");
+    }
+});
